0e46561349246e4703c4fd675d3b4766c216eb29
ashaantania <100705942+ashaantania@users.noreply.github.com>
2022-10-20 21:37:43 +0700

    Add files via upload

---
+++ nomor 2 interface (prak 6).php
@@ -0,0 +1,14 @@
+<!-- Shabinna Rahmadilla Santoso_21091397004 -->
+     
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
